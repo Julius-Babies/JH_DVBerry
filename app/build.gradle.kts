1d3c1203cca0ea02250d99c553e3ea83a92ec121
Nnamllit11 <->
2025-04-12 12:17:56 +0200

    DVB API and dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
